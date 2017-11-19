2eaf383601f01533b6e256db646ad2c18c96aef0
Sven Mawby <fr34kyn01535@bam.yt>
2017-11-19 19:11:23 +0100

    ..

--- unturned-update/www/index.php
+++ unturned-update/www/index.php
@@ -1,6 +1,13 @@
 <?php 
+	$result = new stdClass;
 	$myBuildId = @file_get_contents("/var/www/html/timeupdated");
 	$newBuildId = file_get_contents("http://update.unturned.hosting/timeupdated");
 	if($myBuildId && $myBuildId != $newBuildId) header("HTTP/1.1 500 Internal Server Error");
-	else echo "Up to date.";
- ?>
+	
+	$result->oldBuildId = $myBuildId;
+	$result->newBuildId = $newBuildId;
+	$result->uptodate = $myBuildId && $myBuildId != $newBuildId;
+	$result->updating = !file_exists("/var/www/html/done");
+	
+	echo json_encode($result);
+ ?> 
